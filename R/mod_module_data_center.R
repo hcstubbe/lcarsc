e94e67256fd19f55479520f6baebaa3a1bd81dcd
hcstubbe <herr.hans.s@googlemail.com>
2022-01-27 11:01:00 +0000

    updated data center

--- R/mod_module_data_center.R
+++ R/mod_module_data_center.R
@@ -30,12 +30,17 @@ mod_module_data_center_server <- function(id){
     pool = get_golem_options("pool")
 
     output$responses_table <- DT::renderDataTable({
-      table = dbReadTable(pool, "app_tbl")[,-c(1,2, 4:7)]
-      table <- datatable(table,
-                         rownames = FALSE,
-                         options = list(searching = TRUE, lengthChange = FALSE, pageLength = 10, scrollX = TRUE),
-                         selection = c("single")
+      input_table = dbReadTable(pool, "app_tbl")[,-c(1,2, 4:8)]
+      input_table = t(as.matrix(input_table))
+      input_table_df = data.frame(input_table[-1,])
+      colnames(input_table_df) = input_table[1,-1]
+      input_table_df <- datatable(input_table_df,
+                                  rownames = TRUE,
+                                  options = list(searching = TRUE, lengthChange = FALSE, pageLength = 80, scrollX = TRUE),
+                                  selection = c("single")
       )
+
+      input_table_df
 
     })
 
