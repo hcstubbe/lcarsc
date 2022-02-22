5fb3223ab578c70c765bc646e712b4ca83a42848
hcstubbe <herr.hans.s@googlemail.com>
2022-02-22 05:09:32 +0000

    update

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -11,52 +11,23 @@ rm(list=ls(all.names = TRUE))
 # Document and reload your package
 golem::document_and_reload()
 
-# # Run the application using mariaDB
-# lcarsc::run_app(dbuser = 'user',
-#                 dbpassword = 'user',
-#                 dbhost = 'dbeditor',
-#                 dbname = 'mydbeditor',
-#                 options = list(host = '0.0.0.0',
-#                                port = 3838))
-
 # Run the application using a local SQL database
 # devtools::install_local("/home/rstudio/lcarsM", force = T)
-run_app(options = list(host = '0.0.0.0', port = 3838))
+# run_app(options = list(host = '0.0.0.0', port = 3838))
 
-# pool = pool::dbPool(
-#   drv = RSQLite::SQLite(),
-#   dbname = "mydb.sqlite3",
-#   host = "dbeditor",
-#   username = "user",
-#   password = "user"
-# )
+run_app( config_database_driver = RMariaDB::MariaDB(),
+         config_dbuser = "user",
+         config_dbpassword = "user",
+         config_dbhost = "db",
+         config_dbname = "mydb",
+         ecrf_database_driver = RMariaDB::MariaDB(),
+         ecrf_dbuser = "user",
+         ecrf_dbpassword = "user",
+         ecrf_dbhost = "db",
+         ecrf_dbname = "mydb",
+         options = list(host = '0.0.0.0', port = 3838))
 
-# reset_config = function(){
-#   config_database_driver = RSQLite::SQLite() # RMariaDB::MariaDB()
-#   config_dbuser = "config_user"
-#   config_dbpassword = "config_password"
-#   config_dbhost = "db_config"
-#   config_dbname = "db_config.sqlite3"
-#   pool_config = pool::dbPool(drv = config_database_driver,user = config_dbuser,password = config_dbpassword,host = config_dbhost,db = config_dbname)
-#   RMariaDB::dbRemoveTable(pool_config, "start_config")
-# }
-# reset_config()
-# #
-# config_database_driver = RSQLite::SQLite() # RMariaDB::MariaDB()
-# config_dbuser = "config_user"
-# config_dbpassword = "config_password"
-# config_dbhost = "db_config"
-# config_dbname = "db_config.sqlite3"
-# # pool_config = pool::dbPool(drv = config_database_driver,user = config_dbuser,password = config_dbpassword,host = config_dbhost,db = config_dbname)
-# x = list()
-# for (i in RMariaDB::dbListTables(conn = pool_config)){
-#   xi = RMariaDB::dbReadTable(conn = pool_config, name = i)
-#   x = c(x , list(xi))
-# }
-# names(x) = RMariaDB::dbListTables(conn = pool_config)
-# x
-#
-#
+
 # ecrf_database_driver = RSQLite::SQLite() # RMariaDB::MariaDB()
 # ecrf_dbuser = "default_user"
 # ecrf_dbpassword = "default_password"
