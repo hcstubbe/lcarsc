ed01006341a5134d23e27bb00fa3fb1c5453f44f
hcstubbe <herr.hans.s@googlemail.com>
2022-04-15 09:45:32 +0000

    added return function

--- R/fct_db_read_select.R
+++ R/fct_db_read_select.R
@@ -9,23 +9,30 @@
 
 # Read table and filter
 db_read_select = function(pool, tbl_id, pid_x, filter_deleted_rows = TRUE, use.pid = TRUE, filter_sumitted_rows = FALSE, order.by = NULL, filter_origin = NULL){
+
+
   tab_i = dbReadTable(pool, tbl_id)
+
 
   if(!is.null(order.by)){
     tab_i = tab_i[order(tab_i[,order.by]),]
   }
 
+
   if(filter_deleted_rows == TRUE){
     tab_i = tab_i %>% filter(deleted_row == FALSE)
   }
+
 
   if(filter_sumitted_rows == TRUE){
     tab_i = tab_i %>% filter(submitted_row == TRUE)
   }
 
+
   if(use.pid == FALSE & is.null(filter_origin)){
     return(tab_i)
   }
+
 
   if(!is.null(filter_origin)){
     if(length(filter_origin) > 0){
@@ -36,10 +43,12 @@ db_read_select = function(pool, tbl_id, pid_x, filter_deleted_rows = TRUE, use.p
     return(tab_i)
   }
 
+
   if(length(pid_x) == 0){
     return(NULL)
   }
   tab_i = tab_i %>% filter(pid == pid_x)
 
-  tab_i
+
+  return(tab_i)
 }
