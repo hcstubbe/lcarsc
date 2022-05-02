a097c68ffda1b926fa81d4d53e19ab321226007e
hcstubbe <herr.hans.s@googlemail.com>
2022-05-02 22:03:20 +0000

    clean up

--- tests/testthat/test-fct_makeWidgetList_simple.R
+++ tests/testthat/test-fct_makeWidgetList_simple.R
@@ -3,12 +3,10 @@ test_that("multiplication works", {
   library(shiny)
   widgets_table = readr::read_csv("test_widgets.csv")
   widget_data = widgets_table[widgets_table$widget == TRUE,]
-  widget_list = define_widget(widget_data = widget_data,
+  widget_list = makeWidgetList_simple(widget_data = widget_data,
                               ns = shiny::NS("test"),
                               pid = "test-pid",
-                              tbl_id = "test_tbl_id",
-                              selection = 1:nrow(widget_data))
-
+                              tbl_id = "test_tbl_id")
   expect_equal(length(widget_list), 36)
   for (i in 1:length(widget_list)) {
     expect_equal(class(widget_list[[i]]), "shiny.tag")
