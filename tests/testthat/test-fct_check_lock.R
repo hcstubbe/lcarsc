3ba09d9c38fc236e30a232d656e79e802637478c
hcstubbe <herr.hans.s@googlemail.com>
2022-05-02 21:27:28 +0000

    moved unfinished tests

--- tests/testthat/test-fct_check_lock.R
+++ tests/testthat/test-fct_check_lock.R
@@ -1,3 +1,4 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Checking for locked entries works", {
+  expect_equal(check_lock(data.frame(locked_row = TRUE, editing_user = "test_user"), session = list(ns = shiny::NS("test")), silent = TRUE), TRUE)
+  expect_equal(check_lock(data.frame(locked_row = FALSE, editing_user = "test_user"), session = list(ns = shiny::NS("test")), silent = TRUE), FALSE)
 })
