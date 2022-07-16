b672aefa91a6be9f91052b6d7b98792e50a1c63d
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2022-07-16 23:22:46 +0200

    Update mod_module_edit_tab.R

--- R/mod_module_edit_tab.R
+++ R/mod_module_edit_tab.R
@@ -225,7 +225,9 @@ mod_module_edit_tab_server<- function(id,
     # Add visit selector
     if(editor_filter_visit_id == TRUE){
       filter_visit_data = loadData(get_golem_options("pool_config"), "editor_table_visit")
-      filter_visit_data = dplyr::arrange(filter_visit_data, order)
+      if(!is.null(filter_visit_data$order)){
+        filter_visit_data = dplyr::arrange(filter_visit_data, order)
+      }
       filter_visit_data = filter_visit_data[filter_visit_data$deleted_row == FALSE,]
       filter_visit_choices = c("all_visits", filter_visit_data$visit_id_visits)
       insertUI(
