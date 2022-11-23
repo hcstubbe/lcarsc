b2c274e3d89ff318866f01b3be5838c3ec31856f
Your Name <you@example.com>
2022-11-23 03:51:26 +0000

    update version number

--- R/run_app.R
+++ R/run_app.R
@@ -83,7 +83,7 @@ run_app <- function(
                                                                                    password = config_dbpassword,
                                                                                    host = config_dbhost,
                                                                                    db = config_dbname)),
-                      version = "1.0.50"
+                      version = "1.0.51"
     )
   )
 }
