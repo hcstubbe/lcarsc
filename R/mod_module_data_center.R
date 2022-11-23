bcacc0f5061be78c3ea40f6244f2ccbdff157de5
Your Name <you@example.com>
2022-11-23 00:36:10 +0000

    added chpices from DB

--- R/mod_module_data_center.R
+++ R/mod_module_data_center.R
@@ -19,6 +19,8 @@ mod_module_data_center_ui <- function(id){
                 tabPanel("Overview", br(),
                          actionButton(ns("update_summary"), "Update summary", icon = icon("sync", verify_fa = FALSE)), br(), br(),
                          br(),
+                         mod_module_documentation_summary_graph_ui(ns("module_documentation_summary_graph_1")),
+                         br(),
                          uiOutput(ns("summary"))
                 ),
                 tabPanel("Scientific dataset", br(),
@@ -58,6 +60,8 @@ mod_module_data_center_server <- function(id){
     pool = golem::get_golem_options("pool")
 
 
+    # Start sub-module
+    mod_module_documentation_summary_graph_server("module_documentation_summary_graph_1")
 
     # Render summary data ----
     render_summary = function() {
@@ -67,7 +71,6 @@ mod_module_data_center_server <- function(id){
                                            production_mode = golem::get_golem_options("production_mode"))
       all_visits = widget_data_input$all_visits
       visits_list = all_visits$visit_id[!all_visits$inclusion_other_visit]
-
       total_inclusions = db_read_select(pool,
                                       tbl_id = "inclusion_dataset",
                                       count_rows = TRUE,
@@ -76,8 +79,6 @@ mod_module_data_center_server <- function(id){
                                       filter_deleted_rows = TRUE,
                                       filter_sumitted_rows = FALSE,
                                       select_cols = c("submitted_row", "date_modified"))
-
-      inclusion_series$date_modified = as.Date(inclusion_series$date_modified, format = "%a %b %d %H:%M:%S %Y")
       total_inclusions = as.numeric(total_inclusions[[1]])
 
       #### Render summary ----
