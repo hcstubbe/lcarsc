54e81b5002b3b5a0d59500805aa2c930d62cf619
Your Name <you@example.com>
2022-11-23 02:28:42 +0000

    fixed colors

--- R/mod_module_documentation_summary_graph.R
+++ R/mod_module_documentation_summary_graph.R
@@ -36,7 +36,10 @@ mod_module_documentation_summary_graph_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
     pool = golem::get_golem_options("pool")
-
+    widget_data = load_widget_data(pool_config = golem::get_golem_options("pool_config"),
+                                   production_mode = golem::get_golem_options("production_mode"))
+    all_visits = widget_data$all_visits
+    all_visits[all_visits$visit_id == "vi","visit_id"] = "inclusion_dataset"
 
     get_time_data = function(selected_visits){
       if(length(selected_visits > 0)){
@@ -56,15 +59,19 @@ mod_module_documentation_summary_graph_server <- function(id){
                                           filter_sumitted_rows = FALSE,
                                           select_cols = c("submitted_row", "date_modified"))
         if(nrow(inclusion_series_i) >0){
-          inclusion_series_i$tab_id = i
+          inclusion_series_i$visit_title = all_visits[all_visits$visit_id == gsub("visit_table_", "", i), "visit_title"]
           inclusion_series_i$cumulative_count = 1:nrow(inclusion_series_i)
           inclusion_series = rbind(inclusion_series, inclusion_series_i)
+          inclusion_series$visit_title = factor(inclusion_series$visit_title, levels = all_visits$visit_title)
         }
       }
       inclusion_series$date_modified = as.Date(inclusion_series$date_modified, format = "%a %b %d %H:%M:%S %Y")
-      saveRDS(inclusion_series, "zzz_inclusion_series.RDS")
       return(inclusion_series)
     }
+
+    # Make colors
+    factor_colors = scales::hue_pal()(length(all_visits$visit_title))
+    # names(factor_colors) = all_visits$visit_title
 
 
     output$summary_graph = renderPlot({
@@ -74,12 +81,13 @@ mod_module_documentation_summary_graph_server <- function(id){
             ggplot2::ggplot(data = time_data,
                           ggplot2::aes(x=date_modified,
                                        y=cumulative_count,
-                                       group = tab_id,
-                                       color = tab_id)) +
+                                       group = visit_title,
+                                       color = visit_title)) +
             ggplot2::geom_line() +
             ggplot2::geom_point() +
             ggplot2::theme_minimal() +
-            ggplot2::labs(x = "Date", y = "Count [n]", color = "Visit ID")
+            ggplot2::labs(x = "Date", y = "Count [n]", color = "Visit ID") +
+            ggplot2::scale_color_manual(values = factor_colors)
         }else{
           NULL
         }
