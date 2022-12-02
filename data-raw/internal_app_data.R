3cca36104929c0af9959cd46e5c42b19e436a2f9
Your Name <you@example.com>
2022-12-02 02:41:09 +0000

    basic report

--- data-raw/internal_app_data.R
+++ data-raw/internal_app_data.R
@@ -137,6 +137,3 @@ internal_app_data = list(visits = read.csv('widgets/visits.csv'),
 usethis::use_data(internal_app_data,
                   overwrite = TRUE,
                   internal = TRUE)
-
-# Add rmd template
-golem::use_internal_file("markdown_templates/report.Rmd")
